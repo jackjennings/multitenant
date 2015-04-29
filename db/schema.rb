656bd43e714d6cc5d220617d6548be252b91094d
Jack Jennings <jack@remote.gd>
2015-04-29 16:18:09 -0700

    add units

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429225310) do
+ActiveRecord::Schema.define(version: 20150429231006) do
 
   create_table "buyers", force: :cascade do |t|
     t.string   "first_name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20150429225310) do
     t.string   "email"
     t.string   "slug"
     t.text     "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.integer  "number"
+    t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ Buyer.find_or_create_by slug: 'tenant' do |b|
   b.last_name = "Guy"
   b.email = "foo@bar.com"
 end
+
+Unit.find_or_create_by number: 1 do |u|
+  u.price = 100000.0
+end
