a791cd4242cecb98ae29ba03616b9e58139ec63f
Jack Jennings <jack@remote.gd>
2015-04-29 15:55:20 -0700

    create buyer model

---
+++ db/migrate/20150429225310_create_buyers.rb
@@ -0,0 +1,13 @@
+class CreateBuyers < ActiveRecord::Migration
+  def change
+    create_table :buyers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :slug
+      t.text :data
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ test/models/buyer_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BuyerTest < ActiveSupport::TestCase
+end
