3e0148558cd41dc380a10b84f2bd146de314a25c
Jack Jennings <jack@remote.gd>
2015-04-29 16:07:55 -0700

    add from_param to buyers

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,7 @@
 class Buyer < ActiveRecord::Base
+
+  def self.from_param param
+    find_by_slug! param
+  end
+
 end
--- test/models/buyer_test.rb
+++ test/models/buyer_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
 class BuyerTest < ActiveSupport::TestCase
+
+  test "gets from param by slug" do
+    Buyer.create(slug: 'foo')
+    buyer = Buyer.from_param('foo')
+    assert_not_nil buyer
+  end
+
 end
