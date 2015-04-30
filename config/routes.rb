f5b9d2ba03130f0cd7562664d092b7d7ffcfc304
Jack Jennings <jack@remote.gd>
2015-04-29 17:24:24 -0700

    add multiple root paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 class TenantSubdomainConstraint
-  attr_accessor :excludes, :matches
+  attr_accessor :excludes
 
   def initialize excludes: ['www', '']
     @excludes = excludes
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
   resources :units
 
   scope constraints: TenantSubdomainConstraint.new do
+    # Multiple roots seem to require one to be named
+    root to: 'buyers#show', as: :tenant_root
     resource :buyer, only: :show
   end
+
+  root to: 'units#index'
 
 end
